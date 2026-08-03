0ad9c942aca65d76c4d79cb212be1ea8b5aa9c9f
Samz <39437045+samzwiches@users.noreply.github.com>
2026-08-03 10:08:49 -0400

    Add Virginia Carolinas and Georgia state filters

--- app/data/resources.ts
+++ app/data/resources.ts
@@ -127,16 +127,20 @@ export const resourcePathways = [
 export const stateOptions = [
   { value: "", label: "Any state" },
   { value: "DE", label: "Delaware" },
+  { value: "GA", label: "Georgia" },
   { value: "IN", label: "Indiana" },
   { value: "KY", label: "Kentucky" },
   { value: "ME", label: "Maine" },
   { value: "MA", label: "Massachusetts" },
   { value: "NH", label: "New Hampshire" },
   { value: "NY", label: "New York" },
+  { value: "NC", label: "North Carolina" },
   { value: "OH", label: "Ohio" },
   { value: "PA", label: "Pennsylvania" },
   { value: "RI", label: "Rhode Island" },
+  { value: "SC", label: "South Carolina" },
   { value: "VT", label: "Vermont" },
+  { value: "VA", label: "Virginia" },
   { value: "WV", label: "West Virginia" },
 ] as const;
 
