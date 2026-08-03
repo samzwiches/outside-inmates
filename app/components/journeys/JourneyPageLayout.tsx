1067f74c208b6c7598c38668ed598c5a966716fd
Samz <slduthie@gmail.com>
2026-08-03 11:38:22 -0400

    Remove unused Image component import from JourneyPageLayout

--- app/components/journeys/JourneyPageLayout.tsx
+++ app/components/journeys/JourneyPageLayout.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import type { JourneySlug } from "../../data/journeys";
 import { getJourney, getRelatedJourneys } from "../../data/journeys";
